30ab53af42f9efbc1bd20c2e8e655fe4b62d6a2f
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-09-01 22:09:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "/my-shop/",
+  base: "/shop/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
